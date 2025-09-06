9f2a696a046e5b8ff93b6a5bcbc2c6dac2c847ae
ZalivkoDenis <ZalivkoDenis@gmail.com>
2025-09-06 22:22:35 +0300

    Initial for Python 3.13.7

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    BASE_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     DB_URL: str = f"sqlite+aiosqlite:///{BASE_DIR}/data/db.sqlite3"
-    SECRET_KEY: str
-    ALGORITHM: str
+    SECRET_KEY: str = "SECRETKEY"
+    ALGORITHM: str = "HS256"
 
     model_config = SettingsConfigDict(env_file=f"{BASE_DIR}/.env")
 
